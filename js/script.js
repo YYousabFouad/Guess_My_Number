ab2696cb327c9bd63e0680f594b19a01b13f5087
YYousabFouad <yosab.fouad@gmail.com>
2026-05-01 19:11:30 +0300

    More DRY and update the logic

--- js/script.js
+++ js/script.js
@@ -1,5 +1,13 @@
 'use strict';
 
+const checkingIfScoreIsZero = score => {
+  if (score.textContent <= 0) {
+    score.textContent = 0;
+    document.querySelector('.message').textContent = 'You Lost The Game';
+    document.querySelector('.imoge').textContent = '💀';
+  }
+};
+
 const secretNumber = Math.trunc(Math.random() * 20) + 1;
 
 console.log(secretNumber);
@@ -7,24 +15,28 @@ console.log(secretNumber);
 document.querySelector('.check').addEventListener('click', function () {
   const guess = Number(document.querySelector('.guess').value);
   const message = document.querySelector('.message');
+  const imoge = document.querySelector('.imoge');
+  const score = document.querySelector('.score');
   if (!guess) {
     message.textContent = '⛔ No number!';
+    imoge.textContent = '❌';
   } else if (guess === secretNumber) {
     // if number is right
     message.textContent = '🎉 Congrats , You win! 🎉';
+    imoge.textContent = '✅';
+  } else if (guess > secretNumber) {
+    // if it false but higher
+    message.textContent = '⚠️ Too High, Try again!';
+    imoge.textContent = '⬇️';
+    document.querySelector('.guess').value = '';
+    score.textContent = score.textContent - 1;
+    checkingIfScoreIsZero(score);
   } else {
-    while (secretNumber !== guess) {
-      if (guess > secretNumber) {
-        // if it false but higher
-        message.textContent = 'Too High, Try again!';
-        document.querySelector('.guess').value = '';
-        break;
-      } else {
-        // if it false but lower
-        message.textContent = 'Too low , Try again!';
-        document.querySelector('.guess').value = '';
-        break;
-      }
-    }
+    // if it false but lower
+    message.textContent = '⚠️ Too low , Try again!';
+    imoge.textContent = '⬆️';
+    document.querySelector('.guess').value = '';
+    score.textContent = score.textContent - 1;
+    checkingIfScoreIsZero(score);
   }
 });
