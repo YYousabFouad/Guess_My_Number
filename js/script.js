597ab8c0c8fcb998462564e4dc906c655c632195
YYousabFouad <yosab.fouad@gmail.com>
2026-05-01 17:27:49 +0300

    Apply the principle of DRY

--- js/script.js
+++ js/script.js
@@ -5,15 +5,17 @@ const secretNumber = Math.trunc(Math.random() * 20) + 1;
 console.log(secretNumber);
 
 document.querySelector('.check').addEventListener('click', function () {
-  if (!document.querySelector('.guess').value) {
+  const guess = Number(document.querySelector('.guess').value);
+
+  if (!guess) {
     document.querySelector('.message').textContent = '⛔ No number!';
-  } else if (Number(document.querySelector('.guess').value) === secretNumber) {
+  } else if (guess === secretNumber) {
     // if number is right
     document.querySelector('.message').textContent =
       '🎉 Congrats , You win! 🎉';
   } else {
-    while (secretNumber !== Number(document.querySelector('.guess').value)) {
-      if (Number(document.querySelector('.guess').value) > secretNumber) {
+    while (secretNumber !== guess) {
+      if (guess > secretNumber) {
         // if it false but higher
         document.querySelector('.message').textContent = 'Too High, Try again!';
         document.querySelector('.guess').value = '';
