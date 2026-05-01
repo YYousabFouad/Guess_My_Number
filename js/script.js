6cbcac54d8b7a0df21bfdda4f9c110c063fcee57
YYousabFouad <yosab.fouad@gmail.com>
2026-05-01 04:24:17 +0300

    get the value inside the input guessing box and compare it

--- js/script.js
+++ js/script.js
@@ -1,3 +1,13 @@
 'use strict';
 
-console.log(document.querySelector('.message').textContent);
+const secretNumber = Math.trunc(Math.random() * 20) + 1;
+
+console.log(secretNumber);
+
+if (Number(document.querySelector('.guess').value) === secretNumber) {
+  document.querySelector('.message').textContent = '🎉 Congrats , You win! 🎉';
+} else if (Number(document.querySelector('.guess').value) > secretNumber) {
+  document.querySelector('.message').textContent = 'Too High, Try again!';
+} else {
+  document.querySelector('.message').textContent = 'Too low , Try again!';
+}
