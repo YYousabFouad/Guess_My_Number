13f07a2e9b3c634f89e044edf8f38cbc441180ff
YYousabFouad <yosab.fouad@gmail.com>
2026-05-01 17:33:04 +0300

    More of DRY

--- js/script.js
+++ js/script.js
@@ -6,23 +6,22 @@ console.log(secretNumber);
 
 document.querySelector('.check').addEventListener('click', function () {
   const guess = Number(document.querySelector('.guess').value);
-
+  const message = document.querySelector('.message');
   if (!guess) {
-    document.querySelector('.message').textContent = '⛔ No number!';
+    message.textContent = '⛔ No number!';
   } else if (guess === secretNumber) {
     // if number is right
-    document.querySelector('.message').textContent =
-      '🎉 Congrats , You win! 🎉';
+    message.textContent = '🎉 Congrats , You win! 🎉';
   } else {
     while (secretNumber !== guess) {
       if (guess > secretNumber) {
         // if it false but higher
-        document.querySelector('.message').textContent = 'Too High, Try again!';
+        message.textContent = 'Too High, Try again!';
         document.querySelector('.guess').value = '';
         break;
       } else {
         // if it false but lower
-        document.querySelector('.message').textContent = 'Too low , Try again!';
+        message.textContent = 'Too low , Try again!';
         document.querySelector('.guess').value = '';
         break;
       }
